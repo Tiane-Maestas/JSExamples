c1eda9f03a1de80cfea3a479e27828fee9e8a8d0
Tiane Maestas <tiane.maestas@karemaircraft.com>
2023-08-15 07:44:55 -0700

    Cleaned

--- stdinEx.js
+++ stdinEx.js
@@ -14,7 +14,6 @@ process.stdin.resume();
 // let stdin = process.openStdin();
 // stdin.addListener("data", HandleInput);
 
-// Input format: "motorState motorFrequency"
 function HandleInput(input) {
     input = input.toString();
     console.log("You entered: [" + input + "]");
@@ -23,11 +22,6 @@ function HandleInput(input) {
     console.log(input.charCodeAt(0));
 
     let motorState = Number(input[0]);
-	// if (!(motorState >= motorStateLowLim && motorState <= motorStateUpLim)){ motorState = 1; }		//motorState default 1
-	// console.log('motorState ', motorState) ;	//0:No Command 1:Stop 2:Run
-	// motorFz = Number(readlineSync.question('RPM ')) * motorFzScale ;				//Promt motorFz and converts to number
-	// if (!(motorFz >= motorFzLowLim && motorFz <= motorFzUpLim)){ motorFz = 0; }			//motorFz deafult 0
-	// console.log('motorFz: ', motorFz, '/100 RPM') ;
 }
 
 function delayEx1(time) {
